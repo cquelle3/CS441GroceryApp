7fdb21d084eb2041bfd535162e2cbdb184a1869b
Colin Quelle <cquelle3@binghamton.edu>
2021-03-10 10:44:36 -0500

    Started adding in some code

--- GroceryApp/GroceryApp/ViewController.swift
+++ GroceryApp/GroceryApp/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var enterButton: UIButton!
+    
+    var items = [String]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       // table.delegate = self;
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return items.count;
+    }
+    
+    @IBAction func enterText(_sender: UIButton){
+        if(textField.text?.isEmpty == true){
+            return;
+        }
+        else{
+            items.append(textField.text!);
+            textField.text = "";
+            table.reloadData();
+        }
+    }
+    
+    
+    
+    
+    
 }
 
